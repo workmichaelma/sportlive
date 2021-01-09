5e4b084d3ee7d57aa335d544b86e0ab39f680c5e
Michael Ma <michael.ma@scmp.com>
2021-01-10 01:22:42 +0800

    Build app

---
+++ app/pages/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-dialog v-model="dialog" width="100%">
+    <template v-slot:activator="{ on, attrs }">
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="12">      
+          <div v-for="(matchList, date) in matches" :key="`date__${date}`" class="dateRow">
+            <v-banner single-line class="banner">
+              {{ date }}
+            </v-banner>
+            <div class="cardList">
+              <v-card
+                outlined
+                v-for="(match, key) in matchList"
+                :key="`match_${match.title}_${key}`"
+                v-bind="attrs"
+                v-on:click="link = match.id"
+                v-on="on"
+              >
+                <v-list-item three-line>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline mb-1">
+                      {{ match.title }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>{{ match.league }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </template>
+
+    <v-card>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          text
+          @click="dialog = false; link = false"
+        >
+          關閉
+        </v-btn>
+      </v-card-actions>
+
+      <v-card-text v-if="link"> 
+        <iframe width="100%" :height="height" :src="link" />
+      </v-card-text>
+
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapMutations } from 'vuex'
+import reduce from 'lodash/reduce'
+
+export default {
+  async asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+    try {
+      await store.dispatch('matches/fetchMatches')
+     } catch (err) {
+       console.log(err)
+       return
+     }
+  },
+  mounted () {
+    if (window) {
+      this.height = window.screen.height * 0.7
+      this.width = window.screen.width
+    }
+  },
+  data () {
+    return {
+      dialog: false,
+      link: false,
+      width: 0,
+      height: 0,
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getMatches: 'matches/getMatches',
+    }),
+    matches () {
+      const matches = this.getMatches()
+      return reduce(matches, (obj, match) => {
+        if (obj[match.start]) {
+          obj[match.start].push(match)
+        } else {
+          obj[match.start] = [match]
+        }
+        return obj
+      }, {})
+    }
+  },
+  components: {
+  },
+  watch: {
+    dialog (v) {
+      if (!v) {
+        this.link = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.dateRow
+  > div.v-banner
+    background-color rgb(128 128 128 / 31%)
+    font-size 1.3rem
+    text-align center
+.cardList
+  display flex
+  justify-content flex-start
+  flex-wrap wrap
+  > div
+    margin 15px
+</style>
---
+++ app/store/matches.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+import findIndex from 'lodash/findIndex'
+import get from 'lodash/get'
+import set from 'lodash/set'
+import find from 'lodash/find'
+
+export const state = () => { }
+
+export const getters = {
+  getMatches: (state, getters, rootState, rootGetters) => (featured = true) => {
+    return featured ? state.featuredMatches : state.matches
+  },
+}
+
+export const actions = {
+  async fetchMatches({ getters, rootGetters, commit }) {
+    try {
+      console.log('yo')
+      await axios.get('http://api:3000/').then(({ data }) => {
+        commit('saveMatches', data)
+      }).catch(err => {
+        console.log('catch1', err)
+      })
+    } catch (err) {
+      console.log('catch 2', err)
+    }
+  },
+}
+
+export const mutations = {
+  saveMatches(state, data) {
+    set(state, 'featuredMatches', get(data, 'featuredMatches'))
+    set(state, 'matches', get(data, 'matches'))
+  }
+}
